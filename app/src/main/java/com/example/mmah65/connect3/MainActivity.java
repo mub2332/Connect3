b6c572b94f71b17d6651d0f2ac6c69d61e68e3b4
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-19 08:59:19 +1100

    Initial commit

---
+++ app/src/main/java/com/example/mmah65/connect3/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mmah65.connect3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 0 = yellow, 1 = red
+    int activePlayer = 0;
+
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+        if (counter.getDrawable() == null) {
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360f).setDuration(300);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
